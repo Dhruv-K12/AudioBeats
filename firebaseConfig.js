d0481a8a33133aa28637132a27a2d3f94748ff9d
Dhruv-Ku12 <dmontage000@gmail.com>
2025-07-28 01:38:18 +0530

    added firebaseConfig file

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2nnQo8e6h13mdjJVm9NPcvMcJFuGwR-E",
+  authDomain: "note-app-f189a.firebaseapp.com",
+  databaseURL:
+    "https://note-app-f189a-default-rtdb.firebaseio.com",
+  projectId: "note-app-f189a",
+  storageBucket: "note-app-f189a.firebasestorage.app",
+  messagingSenderId: "689087539165",
+  appId: "1:689087539165:web:04900299ca5a02ec667834",
+  measurementId: "G-98SE2QY35L",
+};
+
+const app = initializeApp(firebaseConfig);
